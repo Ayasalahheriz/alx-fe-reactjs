8a2e228826b920226f93720e92516ebfdda1633d
ayasalah20 <ayasalahyy@gmail.com>
2024-08-25 22:10:35 +0300

    .....

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,16 +2,15 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
-import UserProfile from './components/UserProfile';  // Import the UserProfile component
+import UserProfile from './components/UserProfile'; 
 
-import { useState } from 'react';
+import { useState } from 'react'; 
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); 
 
   return (
     <div className="App">
@@ -21,16 +20,16 @@ function App() {
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <MainContent />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count + 1)}>
           count is {count}
         </button>
         <p>
