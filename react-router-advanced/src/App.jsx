ceb8fca0b51c6a5cc537c84866ef48c8c6ac7bd0
ayasalah20 <ayasalahyy@gmail.com>
2024-09-01 21:36:16 +0300

    ....

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,7 +4,8 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import ProfileDetails from './components/ProfileDetails';
 import ProfileSettings from './components/ProfileSettings';
-import BlogPost from './components/BlogPost';
+import BlogPost from './components/BlogPost'; // This should be used for dynamic routing
+import Post from './components/Post'; // Add this if you also have a Post component
 import ProtectedRoute from './components/ProtectedRoute';
 import NotFound from './components/NotFound';
 
@@ -24,7 +25,8 @@ const App = () => {
           <Route index element={<ProfileDetails />} />
           <Route path="settings" element={<ProfileSettings />} />
         </Route>
-        <Route path="/post/:id" element={<BlogPost />} />
+        <Route path="/post/:id" element={<BlogPost />} /> {/* Dynamic route for posts */}
+        <Route path="/blog/:id" element={<Post />} /> {/* Dynamic route for blog */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
